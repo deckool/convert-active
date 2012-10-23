9a6ee08ac04b59db377dfbbaf2f5243ca166cd30
root <root@deck.(none)>
2012-10-23 22:04:51 +0300

    new convert

---
+++ app/active.js
@@ -0,0 +1,34 @@
+/*
+ * Functie    : getCurrenciesFromXML
+ *
+ * Descriere  : incarca un fisier XML si il transforma in variable
+ *              care sa poate fi folosite in javascript.
+ *
+ * Parametri  : string file
+ *
+ * Returneaza : array data
+ *
+ * Exemple de folosire: getCurrenciesFromXML('data/currencies.xml');
+ */
+
+function getActiveCurrenciesFromXML(file) {
+
+	var currencies = [];
+
+	$.ajax({
+		type : 'GET',
+		url : file,
+		dataType : 'xml',
+		async : false,
+		success : function(data) {
+
+			$(data).find('Active').each(function() {
+				var sym = $(this).attr('currency');
+				currencies.push (sym);
+			});
+		}
+	});
+
+      console.log(currencies);
+return currencies;
+}
